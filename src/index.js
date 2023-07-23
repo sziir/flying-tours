0f01276cc34b9ff4c390dcc3fffe54779edff31a
sziir <moode1121134@gmail.com>
2023-07-23 14:32:19 +0300

    navnar & about company & our services done 1.1

---
+++ src/index.js
@@ -0,0 +1,5 @@
+window.bootstrap = require('bootstrap/dist/js/bootstrap.bundle.js');
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/style.css';
+import '@fortawesome/fontawesome-free/js/all.min';
+import './sass/style.scss';
