11ce2181e4cf9c9a9352d52cbcabce717301bb54
sziir <moode1121134@gmail.com>
2023-07-23 18:35:58 +0300

    turkey tour  done 1.4

--- webpack.config.js
+++ webpack.config.js
@@ -83,6 +83,10 @@ module.exports = {
             filename: 'index.html',
             template: './src/index.html'
         }),
+        new HtmlWebpackPlugin({
+            filename: 'turki.html',
+            template: './src/turki.html'
+        }),
         new MiniCssExtractPlugin({
             filename: 'css/style.css',
 
